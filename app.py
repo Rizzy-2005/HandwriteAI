5fd7a38e1ee9f47f63ad42b94bd58c2eed268e82
Rizzy-2005 <ririshikesh2005@gmail.com>
2025-02-16 10:58:25 +0530

    port number config

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torchvision import transforms
-
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -147,5 +147,6 @@ def about():
     return render_template("about.html")
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000)) # Use Render's PORT or default to 5000
+    app.run(host="0.0.0.0", port=port)
 
