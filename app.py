855b0cfd013cea6eac5b3811278c82c676543cfa
Rizzy-2005 <ririshikesh2005@gmail.com>
2025-08-02 12:09:12 +0530

    Hosting Done

--- app.py
+++ app.py
@@ -129,5 +129,5 @@ def about():
     return render_template("about.html")
 
 #MAIN BLOCK
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=7860)
